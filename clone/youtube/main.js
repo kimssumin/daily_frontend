4e041f68db41361959ceaf8a052b4b7474a60ad2
Sumin Kim <96935132+kimssumin@users.noreply.github.com>
2022-02-03 14:48:58 +0900

    Add files via upload

---
+++ clone/youtube/main.js
@@ -0,0 +1,7 @@
+const moreBtn = docunent.querySelector('.info .metadata .moreBtn')
+const title = document.querySelector('.info .metadata .title')
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+})
